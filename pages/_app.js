7274b1e5bd49e7bd4f16a31cd5373f82d4660211
Sergio Molano <sa.molano@uniandes.edu.co>
2023-11-28 08:07:09 -0500

    Rick And Morty Wiki

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import { motion, AnimatePresence } from 'framer-motion';
+import '../styles/styles.css';
+import '../styles/characterStyles.module.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence>
+      <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
+        pageInitial: {
+          opacity: 0
+        },
+        pageAnimate: {
+          opacity: 1
+        },
+        pageExit: {
+          backgroundColor: 'white',
+          filter: `invert()`,
+          opacity: 0
+        }
+      }}>
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  )
 }
+
+export default App;
